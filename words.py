d69b8bb3889e8ba2ad221da132eb26a3ba8c76a5
Kanak kiraula <kanakkiraula29@gmail.com>
2021-07-08 13:08:12 +0530

    Initial commit

---
+++ words.py
@@ -0,0 +1,5 @@
+words = ["Syntax", "Computer", "Programming", "algorithm", "Binary", "Artificial intelligence", "Template", "array", "integer",
+         "Dennis M. Ritchie", "Bjarne Stroustrup", "function", "debug", "decimal", "floating", "boolean",
+         "Guido van Rossum", "James Gosling", "Brendan Eich", "Tim Berners-Lee", "megabyte", "kilobyte", "byte", "source code", "Variable", "library",
+         "Argument", "Arithmetic operators", "Assignment operators", "Bit", "Low-Level-Language", "mutable", "Linux", "Windows", "Main function", "Scripts",
+         "High-Level-Language", "Conditional statements", "pseudo-code", "flow-chart", "Loops", "Integrated Development Environment", "Java", "assembler", "database", "run-time", "interpreter", "compiler", "Python", "Django"]
